852b009780ff26881c4eb25a9770a01469dc048c
Fellype <fellypenc@gmail.com>
2015-03-13 18:54:42 -0400

    Project struture & Instagram photos with Superbox

---
+++ app/assets/js/application.js
@@ -0,0 +1,17 @@
+$(function() {
+    var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'hackathon',
+    limit: 16,
+    resolution: 'standard_resolution',
+    clientId: '580e9844a5a3431a80641d571f8af4ce',
+    target: 'instagram',
+    template: '<div class="superbox-list"><img src="{{image}}" data-img="{{image}}" alt="" class="superbox-img"></div><!-- -->',
+    after: function() {
+      $('#instagram').SuperBox();
+    }
+});
+    feed.run();
+  
+});
+
