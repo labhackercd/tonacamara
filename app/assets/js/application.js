56e84972427576829e7642b4a6f538b2bc7ab844
Fellype <fellypenc@gmail.com>
2015-03-17 11:14:16 -0400

    Loader.

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -7,11 +7,16 @@ $(function() {
     clientId: '580e9844a5a3431a80641d571f8af4ce',
     target: 'instagram',
     template: '<div class="superbox-list"><img src="{{image}}" data-img="{{image}}" alt="" class="superbox-img"></div><!-- -->',
-    after: function() {
-      $('#instagram').SuperBox();
+    after: function() {      
+      $('#instagram').SuperBox();      
     }
 });
     feed.run();
   
+    $(window).load(function(){
+      $('.load').remove();
+      $('#instagram').fadeIn('slow'); 
+    })
+
 });
 
